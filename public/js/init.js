dcc01326aa9663773842e3aa7812b0ff99c55da6
mbolters <mariematisans@gmail.com>
2019-11-07 21:31:22 -0600

    added css to page and images

--- public/js/init.js
+++ public/js/init.js
@@ -6,3 +6,14 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
