c84bd0677b17c54ec654a8a47ff942dc3de1c23a
Tommy Vinyard <tpvinyard@gmail.com>
2019-11-11 23:55:17 -0600

    clearing tables at first load, adding route for results, adding logic to display data within date range, adding cards to display data based on db

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,34 @@
 var db = require("../models");
 var meteors = require("../public/js/meteor-data.js")
+const moment = require('moment');
 
 console.log(meteors);
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.meteorshowers.bulkCreate(meteors);
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+  app.get("/", async function(req, res) {
+    await db.meteorshowers.destroy({ where: {}, truncate: true
     });
+    await db.meteorshowers.bulkCreate(meteors);
+    let thirtyDaysAgo = moment().subtract(360, 'days').format('YYYY-MM-DD')
+    console.log(thirtyDaysAgo);
+    await db.meteorshowers.findAll({ where: {
+      end_date: {
+        $gte: thirtyDaysAgo
+      }
+    }}).then(function(result) {
+      res.render('index', {
+        index: result
+      })
+    })
   });
+
+  app.get('/event', function(req, res) {
+
+      res.render("results", {
+
+      })
+  })
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
