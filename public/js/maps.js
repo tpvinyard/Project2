572a659c58e75a5e126701832835745b54146f80
Tommy Vinyard <tpvinyard@gmail.com>
2019-11-14 18:14:16 -0600

    add result page data, and format result page

--- public/js/maps.js
+++ public/js/maps.js
@@ -6,7 +6,7 @@ let OPACITY_MAX_PIXELS = 57; // Width of opacity control image
 
 function initializeMap() {
 var mapOptions = {
-    zoom: 4,
+    zoom: 6,
     maxZoom: 17,
     center: new google.maps.LatLng(39.8283, 268.5795),
     scaleControl: true,
@@ -356,7 +356,38 @@ function setMarkers(map, array) {
         zIndex: beach[3]
       });
     }
-  }
+
+    var contentString = `<div style="color: black;">Title: ${marker.title}</div>`
+    // '<div id="content">'+
+    // '<div id="siteNotice">'+
+    // '</div>'+
+    // '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+    // '<div id="bodyContent">'+
+    // '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+    // 'sandstone rock formation in the southern part of the '+
+    // 'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+    // 'south west of the nearest large town, Alice Springs; 450&#160;km '+
+    // '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+    // 'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+    // 'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+    // 'Aboriginal people of the area. It has many springs, waterholes, '+
+    // 'rock caves and ancient paintings. Uluru is listed as a World '+
+    // 'Heritage Site.</p>'+
+    // '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+    // 'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+    // '(last visited June 22, 2009).</p>'+
+    // '</div>'+
+    // '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString,
+        maxWidth: 200
+    });
+
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+}
 
 function createOpacityControl(map) {
         var sliderImageUrl = "opacity-slider3d7.png";
