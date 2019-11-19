2b138350b9bff7fa84846591f59b40e25e261155
Tommy Vinyard <tpvinyard@gmail.com>
2019-11-19 00:51:02 -0600

    search by zip/update map, and info windows on map

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -51,15 +51,6 @@ module.exports = function(app) {
 
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
-
   app.get("/calendar", function(req, res) {
     db.meteorshowers.findAll({}).then(function(result) {
       res.render('calendar', {
